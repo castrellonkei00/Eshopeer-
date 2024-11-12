06dcbbbc7cd1a50255fa4df555c804fed2764386
keira_castrellon_rodriguez <keira.kastrellonr@gmail.com>
2024-11-12 10:37:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Software Punto de Venta" content="">
-    <meta name="Ander Campos" content="">
+    <meta name="Keira Castrellon" content="">
     <title>Inicio | E-Shopper</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
